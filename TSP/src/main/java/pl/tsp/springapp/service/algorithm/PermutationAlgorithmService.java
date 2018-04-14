66cbb077c6a4bddab0a06d122b58aa42f853b8b7
Mateusz <3odaczek@gmail.com>
2018-04-14 19:15:13 +0200

    Permutacje

--- TSP/src/main/java/pl/tsp/springapp/service/algorithm/PermutationAlgorithmService.java
+++ TSP/src/main/java/pl/tsp/springapp/service/algorithm/PermutationAlgorithmService.java
@@ -3,13 +3,42 @@ package pl.tsp.springapp.service.algorithm;
 import pl.tsp.springapp.dto.Point;
 import pl.tsp.springapp.dto.Route;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class PermutationAlgorithmService extends AlgorithmService{
+public class PermutationAlgorithmService extends AlgorithmService {
+  Map<Integer, Route> routesMap = new HashMap<>();
+  int counter = 0;
+
   protected Map<Integer, Route> setRoute(List<Point> points) throws Exception {
     System.out.println("PermutationAlgorithmService");
-    return null;
+
+    if (points.size() > 10)
+      throw new Exception("Za duża liczba punktów");
+
+    perm(points, 0);
+
+    System.out.println(routesMap.size());
+
+    return routesMap;
   }
 
+  private void perm(List<Point> points, int n) {
+    if (n >= points.size() - 1) {
+      Queue<Point> route = new LinkedList<>();
+
+      for (Point tmpPoint : points) {
+        route.offer(tmpPoint);
+      }
+
+      route.offer(route.element());
+      routesMap.put(counter, new Route(route));
+      ++this.counter;
+    } else {
+      for (int i = n; i < points.size(); i++) {
+        Collections.swap(points, i, n);
+        perm(points, n + 1);
+        Collections.swap(points, i, n);
+      }
+    }
+  }
 }
